6ff3d6ca8cf347f3317d910a33530b0b1395ee27
adambreznicky <adam.breznicky@txdot.gov>
2015-06-18 14:04:44 -0500

    cleaned

--- Password_base64.py
+++ Password_base64.py
@@ -3,7 +3,7 @@ __date__ = '4/9/2015'
 __author__ = 'ABREZNIC'
 import base64
 
-password = "February123"
+password = ""
 encoded = base64.b64encode(password)
 decoded = base64.b64decode(encoded)
 print "Password: " + password

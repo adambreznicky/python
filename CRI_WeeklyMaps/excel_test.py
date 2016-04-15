f96cd3a023aa171c8548140b8fb812c5fbf03a4f
Adam Breznicky <adam.breznicky@txdot.gov>
2016-04-15 00:28:12 -0500

    blueberries

---
+++ CRI_WeeklyMaps/excel_test.py
@@ -0,0 +1,38 @@
+__file__ = 'excel_test'
+__date__ = '4/14/2016'
+__author__ = 'ABREZNIC'
+"""
+The MIT License (MIT)
+
+Copyright (c) 2016 Texas Department of Transportation
+Author: Adam Breznicky
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+import arcpy
+
+owssvr_ = "C:\\TxDOT\\CountyRoadInventory\\Book1.xlsx\\County_Road_Certification"
+
+cursor = arcpy.da.SearchCursor(owssvr_, ["*"])
+for row in cursor:
+    print row
+del cursor
+del row
+
+print "that's all folks!!"
